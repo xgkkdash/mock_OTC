bf9516add5bacabf83a8b103bb5c4b9d12b14ab4
xgkkdash <48404201+xgkkdash@users.noreply.github.com>
2021-01-05 11:56:36 +0800

    implement test crud

--- database/mongodb.py
+++ database/mongodb.py
@@ -1,4 +1,6 @@
 import mongoengine
+from models.order import Order
+from models.trade import Trade
 
 
 class MongoDatabase:
@@ -6,22 +8,22 @@ class MongoDatabase:
         self.db_connection = mongoengine.connect(db=db_name, host=db_url)
         self.db_name = db_name
 
-    def get_order(self):
+    def get_order(self, order_id):
         pass
 
-    def save_order(self):
+    def save_order(self, order: Order):
         pass
 
-    def delete_order(self):
+    def delete_order(self, order_id):
         pass
 
-    def get_trade(self):
+    def get_trade(self, trade_id):
         pass
 
-    def save_trade(self):
+    def save_trade(self, trade: Trade):
         pass
 
-    def delete_trade(self):
+    def delete_trade(self, trade_id):
         pass
 
     # Warning: this method should be used carefully and only in the test environment
--- tests/test_crud.py
+++ tests/test_crud.py
@@ -1,47 +1,81 @@
 import pytest
+import uuid
+from database.mongodb import MongoDatabase
+from models.order import Order
+from models.trade import Trade
 
 
-def test_get_invalid_order():
+# this test requires mongo is already start at local pc
+@pytest.fixture()
+def db():
+    db_name = "test_mock_otc"
+    url = "mongodb://localhost:27017/"
+    database = MongoDatabase(db_name, url)
+    yield database
+    database.drop_database()
+
+
+def gen_id():
+    return uuid.uuid4().hex
+
+
+@pytest.fixture()
+def order():
+    return Order("BTC/USD", "buy", 3000, 0.1, gen_id())
+
+
+@pytest.fixture()
+def trade():
+    return Trade("BTC/USD", "buy", 3000, 0.1, gen_id(), gen_id())
+
+
+def test_get_invalid_order(db, order):
     # empty db, gen a invalid order_id and get
-    pass
+    assert not db.get_order(order.order_id)
+    assert not db.get_order("invalid_id")
 
 
-def test_save_order():
+def test_save_order(db, order):
     # gen an order and save it to db
-    pass
+    assert db.save_order(order)
     # get saved order with order_id
-    pass
+    get_o = db.get_order(order.order_id)
+    assert get_o
+    assert get_o.order_id == order.order_id
 
 
-def test_delete_order():
+def test_delete_order(db, order):
     # gen an order and save it to db
-    pass
+    assert db.save_order(order)
     # get saved order with order_id
-    pass
+    assert db.get_order(order.order_id)
     # delete order by passing order_id or order_object
-    pass
+    assert db.delete_order(order.order_id)
     # get by order_id after delete, should get None
-    pass
+    assert not db.get_order(order.order_id)
 
 
-def test_get_invalid_trade():
+def test_get_invalid_trade(db, trade):
     # empty db, gen a invalid trade_id and get
-    pass
+    assert not db.get_trade(trade.trade_id)
+    assert not db.get_order("invalid_id")
 
 
-def test_save_trade():
+def test_save_trade(db, trade):
     # gen a trade and save it to db
-    pass
+    assert db.save_trade(trade)
     # get saved trade with trade_id
-    pass
+    get_trade = db.get_trade(trade.trade_id)
+    assert get_trade
+    assert get_trade.trade_id == trade.trade_id
 
 
-def test_delete_trade():
+def test_delete_trade(db, trade):
     # gen a trade and save it to db
-    pass
+    assert db.save_trade(trade)
     # get saved trade with trade_id
-    pass
+    assert db.get_trade(trade.trade_id)
     # delete trade by passing trade_id or trade_object
-    pass
+    assert db.delete_trade(trade.trade_id)
     # get by trade_id after delete, should get None
-    pass
+    assert not db.get_trade(trade.trade_id)
