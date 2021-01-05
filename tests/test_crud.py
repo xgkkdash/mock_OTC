9a681735dee88b99694e8be7773948f2b718dbdf
xgkkdash <48404201+xgkkdash@users.noreply.github.com>
2021-01-05 10:57:53 +0800

    add base test cases

---
+++ tests/test_crud.py
@@ -0,0 +1,47 @@
+import pytest
+
+
+def test_get_invalid_order():
+    # empty db, gen a invalid order_id and get
+    pass
+
+
+def test_save_order():
+    # gen an order and save it to db
+    pass
+    # get saved order with order_id
+    pass
+
+
+def test_delete_order():
+    # gen an order and save it to db
+    pass
+    # get saved order with order_id
+    pass
+    # delete order by passing order_id or order_object
+    pass
+    # get by order_id after delete, should get None
+    pass
+
+
+def test_get_invalid_trade():
+    # empty db, gen a invalid trade_id and get
+    pass
+
+
+def test_save_trade():
+    # gen a trade and save it to db
+    pass
+    # get saved trade with trade_id
+    pass
+
+
+def test_delete_trade():
+    # gen a trade and save it to db
+    pass
+    # get saved trade with trade_id
+    pass
+    # delete trade by passing trade_id or trade_object
+    pass
+    # get by trade_id after delete, should get None
+    pass
---
+++ tests/test_requests.py
@@ -0,0 +1,24 @@
+import pytest
+
+
+def test_request_buy_quote():
+    pass
+
+
+def test_request_two_way_quote():
+    pass
+
+
+def test_send_order():
+    pass
+
+
+def test_get_order():
+    pass
+
+
+def test_get_trade():
+    # send an order and receive filled back
+    pass
+    # get trade by request with trade_id
+    pass
